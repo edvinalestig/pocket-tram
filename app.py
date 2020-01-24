d2304284470c82d47329506714bf1bfcfc5fc8ab
Edvin Alestig <edvin.alestig@gmail.com>
2020-01-24 23:32:49 +0100

    Works but does not look good

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, send_file
 from vasttrafik import Auth, Reseplaneraren, TrafficSituations
+import json
+from time import strftime
 
 app = Flask(__name__)
 
@@ -10,8 +12,140 @@ auth = Auth(creds[0].strip(), creds[1].strip(), "app")
 rp = Reseplaneraren(auth)
 ts = TrafficSituations(auth)
 
+stopIDs = {
+    "chalmers": 9021014001960000,
+    "lgh": 9021014007171000,
+    "markland": 9021014004760000,
+    "huset1": 9021014005470000,
+    "huset2": 9021014005100000,
+    "jt": 9021014003640000,
+    "mariaplan": 9021014004730000,
+    "kungssten": 9021014004100000,
+    "vasaplatsen": 9021014007300000,
+    "kberget": 9021014004230000
+}
+
 @app.route("/")
 def index():
-    with open("static/index.html", "r") as f:
-        page = f.read()
-    return page
+    return send_file("static/index.html")
+
+@app.route("/request")
+def req():
+    place = request.args.get("place")
+
+    if place == "lgh":
+        return json.dumps({
+            "Ullevi Norra": getDeparture("lgh", "chalmers")
+        })
+
+    elif place == "huset":
+        return json.dumps({
+            "Rengatan": getDeparture("huset1", "kungssten"), 
+            "Nya Varvsallén": getDeparture("huset2", "kungssten")
+        })
+    
+    elif place == "markland":
+        return json.dumps({
+            "Till Kungssten": getDeparture("markland", "kungssten"),
+            "Till Mariaplan": getDeparture("markland", "mariaplan"),
+            "Från Mariaplan": getDeparture("mariaplan", "kungssten")
+        })
+
+    elif place == "jt":
+        return json.dumps({
+            "Mot Chalmers": getDeparture("jt", "chalmers"),
+            "Mot Vasaplatsen": getDeparture("jt", "vasaplatsen"),
+            "Mot Huset": getDeparture("jt", "kberget")
+        })
+
+    elif place == "chalmers":
+        return json.dumps({
+            "Mot Ullevi N": getDeparture("chalmers", "lgh"),
+            "Mot Järntorget": getDeparture("chalmers", "jt"),
+            "Från Järntorget": getDeparture("jt", "huset1"),
+            "Mot Marklandsgatan": getDeparture("chalmers", "markland"),
+            "Från Marklandsgatan": getDeparture("markland", "kungssten")
+        })
+
+    return json.dumps({
+        "test":"test2"
+    })
+
+def getDeparture(fr, to):
+    return clean(rp.departureBoard(
+        id=stopIDs[fr], date=strftime("%Y-%m-%d"), timeSpan=60,
+        time=strftime("%H:%M"), maxDeparturesPerLine=3, 
+        direction=stopIDs[to], needJourneyDetail=0))
+
+def clean(obj):
+    deps = obj.get("DepartureBoard").get("Departure")
+    if deps == None:
+        return []
+    
+    if type(deps) != list:
+        deps = [deps]
+
+    outArr = []
+    for dep in deps:
+        line = dep.get("sname")
+        dest = cut(dep.get("direction"))
+        time = dep.get("time")+getDelay(dep)
+        i = 0
+        while i < len(outArr):
+            if outArr[i].get("line") == line and outArr[i].get("dest") == dest:
+                outArr[i]["time"].append(time)
+                break
+            i += 1
+        else:
+            vitals = {
+                "line": line,
+                "dest": dest,
+                "time": [time],
+                "fgColor": dep.get("fgColor"),
+                "bgColor": dep.get("bgColor")
+            }
+            outArr.append(vitals)
+    return sort_departures(outArr)
+
+def cut(s):
+    return s.split(" via ")[0]
+
+
+def getDelay(dep):
+    if dep.get("cancelled"):
+        return " X"
+
+    # Check if real time info is available
+    tttime = dep.get("time")
+    rttime = dep.get("rtTime")
+    if rttime == None:
+        return ""
+
+    # Convert it all to minutes
+    hour, minutes = rttime.split(":")
+    rtminutes = int(minutes)
+    rtminutes += int(hour) * 60
+
+    hour, minutes = tttime.split(":")
+    ttminutes = int(minutes)
+    ttminutes += int(hour) * 60
+
+    # Time difference:
+    delay = rtminutes - ttminutes
+
+    if delay < -1300:
+        # Past midnight, 24 hours = 1440 min
+        delay += 1440
+    elif delay > 1300:
+        delay -= 1440
+
+    if delay >= 0:
+        return f"+{delay}"
+    else:
+        return str(delay)
+
+def sort_departures(arr):
+    # Sort firstly by line number and secondly by destination
+    sorted_by_destination = sorted(arr, key=lambda dep: dep["dest"])
+    sorted_by_line = sorted(sorted_by_destination, key=lambda dep: int(dep['line']))
+    return sorted_by_line
---
+++ static/script.js
@@ -0,0 +1,94 @@
+let place;
+let departures;
+const choicebox = document.getElementById("setup");
+const depbox = document.getElementById("departures");
+const title = document.getElementById("title");
+const places = {
+    "chalmers": "Chalmers",
+    "lgh": "Lägenheten",
+    "markland": "Marklandsgatan",
+    "jt": "Järntorget",
+    "huset": "Huset"
+}
+
+function reset() {
+    killChildren(depbox);
+    choicebox.classList.remove("hide");
+    title.innerHTML = "Välj plats";
+}
+
+function choose(p) {
+    place = p;
+    title.innerHTML = places[place];
+    choicebox.classList.add("hide");
+    getDepartures();
+}
+
+function killChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function addDeparture(obj) {
+    console.log(obj);
+    for (let box of Object.keys(obj)) {
+        createBox(box, obj[box]);
+    }
+}
+
+function createBox(title, contents) {
+    let box = document.createElement("div");
+    depbox.appendChild(box);
+    let h3 = document.createElement("h3");
+    h3.innerHTML = title;
+    h3.classList.add("grouptitle");
+    box.appendChild(h3);
+
+    for (let d of contents) {
+        times = d.time;
+        let row = createRow(
+            d.line, d.dest, times[0], (times.length > 1) ? times[1]:"", 
+            (times.length > 2) ? times[2]:"", "background-color: " + 
+            d.fgColor + "; color: " + d.bgColor + ";");
+        box.appendChild(row);
+    }
+
+}
+
+function createRow(line, dir, d1, d2, d3, style) {
+    let row = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
+    td1.innerHTML = line;
+    td2.innerHTML = dir;
+    td3.innerHTML = d1;
+    td4.innerHTML = d2;
+    td5.innerHTML = d3;
+    td1.classList.add("line");
+    td2.classList.add("direction");
+    td3.classList.add("time");
+    td4.classList.add("time");
+    td5.classList.add("time");
+    td1.style = style;
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+    row.appendChild(td5);
+    return row;
+}
+
+function getDepartures() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/request?place="+place);
+    xhr.onload = () => {
+        departures = xhr.response
+        addDeparture(xhr.response);
+    }
+    xhr.responseType = "json";
+    xhr.send();
+}
