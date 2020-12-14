d2b3f0b4753f10a43ac5a58bdaf956a0f6e8de8f
Edvin Alestig <edvin.alestig@gmail.com>
2020-12-14 21:39:07 +0100

    Finished stop & line pages

--- utilityPages.py
+++ utilityPages.py
@@ -1,6 +1,5 @@
 from flask import send_file
 import json
-import codecs
 from datetime import datetime
 import dateutil.tz as tz
 
@@ -32,20 +31,31 @@ class UtilityPages:
         if type(departures) == dict:
             departures = [departures]
 
+        types = {
+            "BUS": "🚌",
+            "TRAM": "🚋",
+            "VAS": "Västtåg",
+            "LDT": "Fjärrtåg",
+            "REG": "Regionaltåg",
+            "BOAT": "⛴",
+            "TAXI": "🚕"
+        }
+
         html = '<!DOCTYPE html>\n<html lang="sv">\n<head>\n<meta charset="UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<title>Avgångar</title>\n</head>\n<body>'
+        html += "<a href='/utilities'>Till sökruta</a>"
         html += f"<h1>{stopName}, {depTime}, {depDate}</h1>"
         html += "\n<table>"
         html += "\n<tr><th>Linje</th><th>Destination</th><th>TT-tid</th><th>RT-tid</th><th>Låggolv</th><th>Läge</th><th>Typ</th><th>Inställd</th><th>Bokas?</th><th>Mer info</th></tr>"
         depRows = [(
-            f"\n<tr>"
-            f"<td style='background-color: {dep.get('fgColor')}; color: {dep.get('bgColor')}; text-align: center;'>{dep.get('sname')}</td>"
+            f'\n<tr style="border: 5px solid red;">'
+            f"<td style='background-color: {dep.get('fgColor')}; color: {dep.get('bgColor')}; text-align: center; border: 1px {dep.get('stroke')} {dep.get('bgColor')};'>{dep.get('sname')}</td>"
             f"<td>{dep.get('direction')}</td>"
             f"<td style='text-align: center;'>{dep.get('time')}</td>"
             f"<td style='text-align: center;'>{dep.get('rtTime') if dep.get('rtTime') else '-'}</td>"
             f"<td style='text-align: center;'>{'♿' if dep.get('accessibility') == 'wheelChair' else '❌'}</td>"
             f"<td style='text-align: center;'>{dep.get('track')}</td>"
-            f"<td style='text-align: center;'>{dep.get('type')}</td>"
-            f"<td style='text-align: center;'>{'Ja' if dep.get('cancelled') else 'Nej'}</td>"
+            f"<td style='text-align: center;'>{types.get(dep.get('type'))}</td>"
+            f"<td style='text-align: center;'>{'🔴' if dep.get('cancelled') else '🟢'}</td>"
             f'<td style="text-align: center;">{"Ja" if dep.get("booking") else "Nej"}</td>'
             f"<td style='text-align: center;'><a href='/depInfo?ref={dep.get('JourneyDetailRef').get('ref').split('?ref=')[1]}'>Mer info</a></td>"
             f"</tr>"
@@ -63,18 +73,23 @@ class UtilityPages:
         if type(col) == dict:
             color = col.get("bgColor")
             bg = col.get("fgColor")
+            stroke = col.get("stroke")
         else:
             idx = int(stop.get("routeIdx"))
             color = "red"
             bg = "hotpink"
 
-        return f'color: {color}; background-color: {bg};'
+        return f'color: {color}; background-color: {bg}; border: 1px {stroke} {color};'
 
     def getLine(self, dep, stop):
         line = dep.get("JourneyName")
         if type(line) == dict:
             return line.get("name")
         else:
+            idx = int(stop.get("routeIdx"))
+            for l in reversed(line):
+                if int(l.get("routeIdxFrom")) <= idx and int(l.get("routeIdxTo")) >= idx:
+                    return l.get("name")
             return "Linje"
 
     def getDestination(self, dep, stop):
@@ -82,6 +97,10 @@ class UtilityPages:
         if type(dest) == dict:
             return dest.get("$")
         else:
+            idx = int(stop.get("routeIdx"))
+            for d in reversed(dest):
+                if int(d.get("routeIdxFrom")) <= idx and int(d.get("routeIdxTo")) >= idx:
+                    return d.get("$")
             return "Destination"
     
     def depInfo(self, args):
@@ -93,9 +112,15 @@ class UtilityPages:
         with open("dep.json", "w") as f:
             f.write(json.dumps(dep))
 
-        html = '<!DOCTYPE html>\n<html lang="sv">\n<head>\n<meta charset="UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<title>Avgångar</title>\n</head>\n<body>'
-        html += "<table>\n"
-        # header
+        html = (
+            '<!DOCTYPE html>\n<html lang="sv">\n<head>\n<meta charset="UTF-8">\n'
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
+            '<title>Avgångar</title>\n'
+            '<style>td, tr {margin: 0px; padding-left: 5px; padding-right: 5px;}'
+            'tr:nth-of-type(even) {border-bottom: 1px solid black;}'
+            'table {border-collapse: collapse;}</style>'
+            '</head>\n<body>\n<table>\n')
+        # header here
         stops = [(
             f'<tr>'
             f'<td rowspan="2" style="{self.getStyle(dep, stop)}">{self.getLine(dep, stop)}</td>'
@@ -103,12 +128,15 @@ class UtilityPages:
             f'<td>{stop.get("name").split(", ")[0]}</td>'
             f'<td>Ank. {stop.get("arrTime") if stop.get("arrTime") else "-"}</td>'
             f'<td>Avg. {stop.get("depTime") if stop.get("depTime") else "-"}</td>'
+            f'<td>Läge {stop.get("track")}</td>'
+            f'<td>Hpl-ID: {stop.get("id")}</td>'
             f'</tr>'
             f'<tr>'
             f'<td>{stop.get("name").split(", ")[1]}</td>'
             f'<td>RT ank. {stop.get("rtArrTime") if stop.get("rtArrTime") else "-"}</td>'
             f'<td>RT avg. {stop.get("rtDepTime") if stop.get("rtDepTime") else "-"}</td>'
-            f'<td>Läge {stop.get("track")}</td>'
+            f'<td>Lat: {stop.get("lat")}</td>'
+            f'<td>Lon: {stop.get("lon")}</td>'
             f'</tr>'
         ) for stop in dep.get("Stop")]
         
